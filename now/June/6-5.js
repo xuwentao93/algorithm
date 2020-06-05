33ef117953c22464e9ab5d224b1ce3b4b09d7c65
xuwentao93 <a15157756529@gmail.com>
2020-06-05 17:10:13 +0800

    6-5

--- now/June/6-5.js
+++ now/June/6-5.js
@@ -96,4 +96,12 @@ var lengthOfLongestSubstring = function(s) {
   return Math.max(str.length, ans);
 };
 
-console.log(lengthOfLongestSubstring("dvdf"));
+// l - 88.
+
+var merge = function(nums1, m, nums2, n) {
+  let p1 = m - 1, p2 = n - 1;
+  for (let i = m + n - 1; i >= 0; i--) {
+    if ((nums1[p1] > nums2[p2] || p2 === -1) && p1 !== -1) nums1[i] = nums1[p1--];
+    else nums1[i] = nums2[p2--];
+  }
+};
