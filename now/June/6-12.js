c10418be2042539540a39fc140bd0e697d0a51e5
xuwentao93 <a15157756529@gmail.com>
2020-06-12 11:33:18 +0800

    6-12

--- now/June/6-12.js
+++ now/June/6-12.js
@@ -15,3 +15,38 @@ var lengthOfLIS = function(nums) {
   }
   return ans;
 };
+
+// greedy + binary search. time: O(nlogn). space: O(1).
+var lengthOfLIS = function(nums) {
+  if (nums.length <= 1) return nums.length;
+  let len = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[len - 1]) {
+      nums[len++] = nums[i];
+    } else {
+      if (len === 1) {
+        nums[0] = nums[i];
+      } else {
+        if (nums[i] < nums[0]) nums[0] = nums[i];
+        else if (nums[i] < nums[len - 1] && nums[i] > nums[len - 2]) nums[len - 1] = nums[i];
+        else {
+          let start = 0;
+          let end = len - 1;
+          while (start < end) {
+            const mid = ~~ (start + end) >> 1;
+            if (nums[i] > nums[mid]) start = mid + 1;
+            else if (nums[i] < nums[mid - 1]) end = mid - 1;
+            else if (nums[i] === nums[mid]) break;
+            else {
+              nums[mid] = nums[i];
+              break;
+            }
+          }
+        }
+      }
+    }
+  }
+  return len;
+}
+
+lengthOfLIS([3,5,6,2,5,4,19,5,6,7,12]);
