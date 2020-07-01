57742377decf2a6201bee608786c98a4cd9d8725
xuwentao93 <a15157756529@gmail.com>
2020-07-01 10:47:00 +0800

    7-1

--- now/July/7-1.js
+++ now/July/7-1.js
@@ -11,3 +11,16 @@ var levelOrderBottom = function(root) {
     dfs(root.right, level + 1);
   }
 }
+
+// l - 110.
+var isBalanced = function(root) {
+  return recursion(root) !== -1;
+  function recursion(tree) {
+    if (tree === null) return 0;
+    let left = recursion(root.left);
+    if (left === -1) return -1;
+    let right = recursion(root.right);
+    if (right === -1) return -1;
+    return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+  } 
+}
